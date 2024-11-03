674c758f9f4dc1b493297848b581133b06b9713c
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-04 01:07:45 +0530

    Created signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { signup } from '../controllers/auth.controller.js';
+import { signin } from '../controllers/auth.controller.js';
 const router=express();
 router.post('/signup',signup);
+router.post('/signin',signin);
 export default router;
