846538fc285aa5e4e0647464028efefb5a217a4c
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-15 20:54:23 +0530

    created profile update ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,18 +16,15 @@ const Dashboard = () => {
   },[location.search]);
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className="">
-        {/*sidebar*/}
-        <DashSidebar />
-      </div>
-      <div className="">
-        {/*profile*/}
-
-        {tab==='profile'&&<DashProfile />}
-      </div>
-      </div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className='md:w-56'>
+      {/* Sidebar */}
+      <DashSidebar />
+    </div>
+    {/* profile... */}
+    {tab === 'profile' && <DashProfile />}
+  </div>
+);
 }
 
 export default Dashboard
