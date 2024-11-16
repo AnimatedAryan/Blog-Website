6b9dff3f184cc7b1f22d3ecf46e8cd7f5b062450
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-16 23:58:22 +0530

    added comments functionality to the website

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 import { verifyToken } from '../utils/verifyUser.js';
-import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
+import {deleteUser,getUser,getUsers,signout,test,updateUser} from '../controllers/user.controller.js';
 const router=express.Router();
 router.get('/test', test);
 router.delete('/delete/:userId', verifyToken,deleteUser);
 router.put('/update/:userId', verifyToken,updateUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
