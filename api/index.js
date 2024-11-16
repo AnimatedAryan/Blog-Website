efc6e268ee2105902516fca7fa56496cb91a45d9
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-16 14:50:09 +0530

    created post api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
+app.use('/api/post', postRoutes);
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
     const message=err.message||'Internal Server Error'; 
