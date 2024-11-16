9cfc5b655a287f1b10b72d67d2bdd4f8d54fa99a
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-16 23:24:36 +0530

    Delete user functionality for admin

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
-
 import React, { useEffect,useState} from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 const Dashboard = () => {
   const location=useLocation();
   const [tab,setTab]=useState('');
@@ -29,6 +29,8 @@ const Dashboard = () => {
     {tab === 'profile' && <DashProfile />}
     {/* posts... */}
     {tab === 'posts' && <DashPosts />}
+    {/* users */}
+    {tab === 'users' && <DashUsers />}
   </div>
 </div>
 );
