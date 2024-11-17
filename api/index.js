7553c9fd001da4183c0a42c5a01513a0173e0b2a
Aryan Birkhani <aryanbirkhani214@gmail.com>
2024-11-17 17:49:47 +0530

    added projects

--- api/index.js
+++ api/index.js
@@ -6,12 +6,13 @@ import postRoutes from './routes/post.route.js';
 import authRoutes from './routes/auth.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log('Mongodb is connected')
 }).catch((err)=>{console.log(err)})
-
+const _dirname=path.resolve();
 const app=express();
 app.listen(3000,()=>{
     console.log('server is runing');
@@ -22,6 +23,10 @@ app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
     const message=err.message||'Internal Server Error'; 
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 const about = () => {
   return (
-    <div>about</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+    <div className='max-w-2xl mx-auto p-3 text-center'>
+      <div>
+        <h1 className='text-3xl font font-semibold text-center my-7'>
+          About Aryan's Blog
+        </h1>
+        <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <p>
+            Welcome to Aryans's Blog! This blog was created by Aryan Birkhani
+            as a personal project to share his thoughts and ideas with the
+            world.
+          </p>
+          <p>
+          Discover the perfect blend of inspiration and flavor! Here, we share uplifting lifestyle tips, motivational insights, 
+          and mouthwatering recipes to elevate your everyday life. 
+          </p>
+          <p>
+          Whether you're seeking encouragement to chase your dreams, looking for ways to bring balance to your routine, or searching for a delicious new dish to try, you've come to the right place. Join us on this journey of growth, positivity, and culinary creativity. Let's inspire and savor life's best moments together!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default about
